dc03bc8705a07b83596d2713c709ddcd2f387ad9
Chahat Mittal <chahat.mittal@hcltech.com>
2025-11-25 18:18:29 +0530

    Updated the main.tf

--- modules/Resource1/main.tf
+++ modules/Resource1/main.tf
@@ -9,10 +9,20 @@ data "azurerm_resource_group" "existing_rg" {
 }
 
 # Create Storage Account in existing RG
+
 resource "azurerm_storage_account" "storage1" {
-  name                     = "storageacctresource1"
+  name                     = var.storage_account_name
   resource_group_name      = data.azurerm_resource_group.existing_rg.name
   location                 = data.azurerm_resource_group.existing_rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+
+  
+
+  network_rules {
+    default_action             = "Deny"
+    bypass                     = ["AzureServices"]
+    virtual_network_subnet_ids = []
+  }
 }
+
